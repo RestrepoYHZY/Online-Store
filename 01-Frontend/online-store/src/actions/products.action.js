6d465e3596ef2672bec5099a43560d60ca3388bd
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-10-20 18:18:54 -0500

    delete in products

--- 01-Frontend/online-store/src/actions/products.action.js
+++ 01-Frontend/online-store/src/actions/products.action.js
@@ -4,3 +4,15 @@ const path="/products"
 
 export const getProducts = async ()=> 
 await http_service.get(path);
+
+export const getProductById = async (id)=>
+  await http_service.get(`${path}/${id}`);
+
+  export const postProducts= async (data)=>
+  await http_service.post(path, data);
+
+  export const putProducts = async (id, data)=>
+  await http_service.put(`${path}/${id}`, data);
+
+  export const deleteProducts= async (id)=>
+  await http_service.delete(`${ path }/${ id }`);
