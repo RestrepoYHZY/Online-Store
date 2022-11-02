dc8354efd653bec7cc5b099de539f30698440a4c
Karen Chica <Karen.Chica@teaminternational.com>
2022-11-02 14:03:30 -0500

    style on invoice input

--- 01-Frontend/online-store/src/components/Invoice/FormInvoice/FieldsForm.jsx
+++ 01-Frontend/online-store/src/components/Invoice/FormInvoice/FieldsForm.jsx
@@ -1,59 +1,89 @@
-import { Grid  } from "@mui/material";
+import { Grid, InputLabel, Menu, MenuItem, Select, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
-
 
 const FieldsForm = ({ tools }) => {
   return (
     <>
-      
       <Grid item xs={10}>
-        <Grid container spacing ={ 2 }>
+        <Grid container spacing={2}>
           <Grid item xs={4}>
             <Stack>
-            <label htmlFor="date">Date: </label>
-            <input type="date" name="date"  onChange={ e=>tools.setDate(e.target.value) } value={ tools.date }/>
-            </Stack>
-          </Grid>
-          <Grid item xs={4}>
-          <Stack>
-            <label htmlFor="customer">Customer: </label>
-            <select  name="customer" onChange={ e=>tools.setCustomer(e.target.value) } value={ tools.customer }>
-              <option value="">Choose customer</option>
-              { tools.customers.map(item=>(
-              <option key={ item.id } value={item.id }> { `${ item.name } ${ item.lastName }` }</option>
-              ))}
-
-            </select>
+              <InputLabel htmlFor="date">Date: </InputLabel>
+              <TextField
+                type="date"
+                name="date"
+                size="small"
+                onChange={(e) => tools.setDate(e.target.value)}
+                value={tools.date}
+              />
             </Stack>
           </Grid>
           <Grid item xs={4}>
             <Stack>
-            <label htmlFor="product">Product: </label>
-            <select  name="product" onChange={ e=>tools.getAProduct(e.target.value) } value={ tools.product }>
-              <option value="">Choose Product</option>
-              { tools.products.map(item=>(
-              <option key={ item.id } value={item.id }> { item.nameProduct }</option>
-              ))}
-
-            </select>
+            <InputLabel htmlFor="date">Customers: </InputLabel>
+              <Select name="customer"   size="small"
+                onChange={(e) => tools.setCustomer(e.target.value)}
+                value={tools.customer}>
+               
+                {tools.customers.map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {" "}
+                    {`${item.name} ${item.lastName}`}
+                  </MenuItem>
+                ))}
+              </Select>
+             
             </Stack>
           </Grid>
           <Grid item xs={4}>
             <Stack>
-            <label htmlFor="price">Price: </label>
-            <input name="price" type="number" value={ tools.price } disabled/>
+              <InputLabel id="product">Product: </InputLabel>
+            
+              <Select
+                name="product" size="small"
+                onChange={(e) => tools.getAProduct(e.target.value)}
+                value={tools.product}
+               
+              >
+                
+                {tools.products.map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {" "}
+                    {item.nameProduct}
+                  </MenuItem>
+                ))}
+              </Select>
             </Stack>
           </Grid>
           <Grid item xs={4}>
-          <Stack>
-            <label htmlFor="amount">Amount: </label>
-            <input name="amount" type="number" onChange={ e=>tools.multiplyPrice(e.target.value) } value={ tools.amount } min={ 0 }/>
+            <Stack>
+              <InputLabel id="price">Price: </InputLabel>
+              <TextField name="price" type="number"  size="small" value={tools.price} disabled />
             </Stack>
           </Grid>
           <Grid item xs={4}>
-          <Stack>
-            <label htmlFor="subTotal">SubTotal: </label>
-            <input type="number" name="subTotal"  value={ tools.subTotal }disabled/>
+            <Stack>
+              <InputLabel id="amount">Amount: </InputLabel>
+              <TextField
+                name="amount"
+                type="number"
+                size="small"
+                onChange={(e) => tools.multiplyPrice(e.target.value)}
+                value={tools.amount}
+                min={0}
+              />
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
+            <Stack>
+              <InputLabel htmlFor="subTotal">SubTotal: </InputLabel>
+              <TextField
+                type="number"
+                name="subTotal"
+                size="small"
+                value={tools.subTotal}
+                disabled
+              />
             </Stack>
           </Grid>
         </Grid>
